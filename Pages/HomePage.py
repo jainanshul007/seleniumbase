a9116045c61f66eb5236c2503b503eddbac7a891
Anshul Jain18 <anshuj18@in.ibm.com>
2021-12-26 18:02:58 +0530

    added changes

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -1,14 +1,12 @@
-# from Pages.BasePage import BasePage
-# from selenium.webdriver.common.by import By
-#
-# class HomePage(BasePage):
-#
-#
-# """"""Home page object""""""
-#     EMAIL = (By.ID, "ap_email")
-#     SEARCHBOX = (By.ID , "twotabsearchtextbox")
-#     SEARCHBTN = (By.ID , "nav-search-submit-button")
+from Pages.BasePage import BasePage
+from selenium.webdriver.common.by import By
 
 
+class HomePage(BasePage):
 
 
+    '''Home page object'''
+
+    EMAIL = (By.ID, "ap_email")
+    SEARCHBOX = (By.ID, "twotabsearchtextbox")
+    SEARCHBTN = (By.ID, "nav-search-submit-button")
